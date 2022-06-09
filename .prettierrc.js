2c72ce542e1832d8e8e9550d45dd6675b1d2b9e6
Akhil Vaidya <vaidyakhil@BGVMC580.local>
2022-06-09 20:36:23 +0530

    initialized repo

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	singleQuote: true,
+	arrowParens: 'always',
+	printWidth: 120,
+	endOfLine: 'auto',
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+};
---
+++ src/screens/registry.tsx
@@ -0,0 +1,11 @@
+import LoginScreen from './login';
+import SignupScreen from './signup';
+import { SCREENS } from './screen_list';
+import { AppScreens } from './types';
+
+const SCREEN_REGISTRY: { [key in SCREENS]: AppScreens } = {
+	[SCREENS.SIGNUP]: SignupScreen,
+	[SCREENS.LOGIN]: LoginScreen,
+};
+
+export { SCREEN_REGISTRY };
